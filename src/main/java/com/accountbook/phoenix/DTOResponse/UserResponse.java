5ff09879d91bd63d0adae3e0ddf77b9f091dbbbf
0110S <sanjay.dudekula@imaginnovate@.com>
2023-03-10 23:15:40 +0530

    v.08

--- src/main/java/com/accountbook/phoenix/DTOResponse/UserResponse.java
+++ src/main/java/com/accountbook/phoenix/DTOResponse/UserResponse.java
@@ -1,18 +1,19 @@
 package com.accountbook.phoenix.DTOResponse;
 
-import com.accountbook.phoenix.DTO.UserData;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.File;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class UserResponse {
 
-    private String message;
-    private UserData response;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private File profilePic;
+    private boolean following;
 }
-
